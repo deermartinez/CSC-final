60a8425cfe1f9c093183eabc4724d6697ac786ae
Destynee Martine <deermartinez8@gmail.com>
2021-01-12 17:05:14 -0700

    add

--- tc_cart.js
+++ tc_cart.js
@@ -31,47 +31,21 @@ var cartHTML =
 
 
 
-
-
-
 </table>
 
 // make a for loop here, going through the entries int he item array,
 // each time it loops, execute the following commands:
 //add the following to the cartHTML
-for (i = 0; i < item.length; i++) {
-   ul.append(
-   <tr> 
-      <td> <img src = 'tc_item.png' alt='item' > </img> </td>
+for (i = 0; i < item.length; i++){
+   //add html code to cart var
+   <tr>
+      <td><img src='tc_item.png' alt='item' /></td>
+      <td>description</td>
+      <td>$price</td>
+      <td>quantity</td>
+      var itemCost = (price * quantity);
+      <td>$cost</td>
    </tr>
 
 
-ul.append(
-   // add HTML code to the cartHTML variable to display the description price and wuantity of the item
-<td>description</td>
-<td>$price</td>
-<td>quantity</td>
-)
-
-   ) //where item is the current value from the item array'
-
-
-
-
-
-
-
-
-}
-
-
-
-//description is the current value from itemDescriptoiio,
-//price is current value from itemPrice preceded by $
-//quantity is current value from itemQuan
-
-var itemCost = price * quantity; //for current item
-
-
-
-
+} 
